b604eb53387f71a4aa4bb5bf1796dac1bf4db1fd
Asaph <easaph25@gmail.com>
2026-03-05 21:36:48 +0100

    first commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+dotenv.config({ path: './config.env' });
+
+import app from './app.js';
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
+  console.log(`App is listening on port ${PORT}`);
+});
